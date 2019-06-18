663832b595b2f9094076ccf80fc74330781d3e81
aereal <aereal@aereal.org>
2019-06-18 23:37:37 +0900

    add blank implementation

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
-import * as functions from 'firebase-functions';
+import { region } from "firebase-functions";
+import admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const api = region("asia-northeast1").https.onRequest(
+  async (_, res): Promise<void> => {
+    res.send({ ok: true, ane: [] });
+  }
+);
